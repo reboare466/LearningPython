5e60dbd733f12eef6ffa9f048aee86439bed7a54
reboare466 <avmurray2@gmail.com>
2016-04-07 14:50:02 +1000

    Exercises 24-28
    My recent inactivity was due to moving house but now I'm getting back
    into it and hopefully can continue to juggle this learning with other
    stuff.

---
+++ ex27.py
@@ -0,0 +1,34 @@
+# ex27
+
+# not FALSE = TRUE
+# not TRUE = FALSE
+
+# TRUE or FALSE = TRUE
+# TRUE or TRUE = TRUE
+# FALSE or TRUE = TRUE
+# FALSE or FALSE = TRUE
+
+# TRUE and FALSE = FALSE
+# TRUE and TRUE = TRUE
+# FALSE and TRUE = FALSE
+# FALSE and FALSE = FALSE
+
+# not (TRUE or FALSE) = FALSE
+# not (TRUE or TRUE) = FALSE
+# not (FALSE or TRUE) = FALSE
+# not (FALSE or FLASE) = TRUE
+
+# not (TRUE and FALSE) = TRUE
+# not (TRUE and TRUE) = FALSE
+# not (FALSE and TRUE) = TRUE
+# not (FALSE and FALSE) = TRUE
+
+# 1 != 0 = TRUE
+# 1 != 1 = FALSE
+# 0 != 1 = TRUE
+# 0 != 0 = FALSE
+
+# 1 == 0 = FALSE
+# 1 == 1 = TRUE
+# 0 == 1 = FALSE
+# 0 == 0 = TRUE
---
+++ ex28.py
@@ -0,0 +1,47 @@
+# ex28
+
+# Answers as in my head:
+
+# 1. TRUE
+# 2. FALSE
+# 3. FALSE
+# 4. TRUE
+# 5. TRUE
+# 6. TRUE
+# 7. FALSE
+# 8. TRUE
+# 9. FALSE
+# 10. FALSE
+# 11. TRUE
+# 12. FALSE
+# 13. TRUE
+# 14. FALSE
+# 15. FALSE
+# 16. FALSE
+# 17. TRUE
+# 18. TRUE
+# 19. FALSE
+# 20. FALSE
+
+# Python's output:
+
+print(True and True) # 1.
+print(False and True) # 2.
+print(1 == 1 and 2 == 1) # 3.
+print("test" == "test") # 4.
+print(1 == 1 or 2 != 1) # 5.
+print(True and 1 == 1) # 6.
+print(False and 0 != 0) # 7.
+print(True or 1 == 1) # 8.
+print("test" == "testing") # 9.
+print(1 != 0 and 2 == 1) # 10.
+print("test" != "testing") # 11.
+print("test" == 1) # 12.
+print(not(True and False)) # 13.
+print(not(1 == 1 and 0 != 1)) # 14.
+print(not (10 == 1 or 1000 == 1000)) # 15.
+print(not(1 != 10 or 3 == 4)) # 16.
+print(not("testing" == "testing" and "Zed" == "Cool Guy")) # 17.
+print(1 == 1 and (not ("testing" == 1 or 1 == 0))) # 18.
+print("chunky" == "bacon" and (not (3 == 4 or 3 == 3))) # 19.
+print(3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")))
